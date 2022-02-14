29b80d53f01f194af602f1e2f26fb4b1bf562013
joshmagic95 <99381454+joshmagic95@users.noreply.github.com>
2022-02-14 10:32:32 +0800

    Add comment lines

--- src/scannermethod/ScannerMethod.java
+++ src/scannermethod/ScannerMethod.java
@@ -4,16 +4,20 @@ public class ScannerMethod {
     public static void main(String[] args) {
         // creating Scanner class object
         Scanner scan = new Scanner (System.in);
+        // reading value from the user
         System.out.print("Enter the number: ");
-        int num = scan.nextInt();
-        findEvenOdd(num);
+        int num = scan.nextInt(); // method calling
+        findEvenOdd(num); // pass parameter num
     }
     
+    // user defined method
     public static void findEvenOdd(int num){
         // method body
-        if (num % 2 == 0)
+        if (num % 2 == 0){
             System.out.println(num+" is even");
-        else
+        }
+        else {
             System.out.println(num+" is odd");
+        }
     }
 }
